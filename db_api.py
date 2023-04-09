cb3c055703bed3dad23437bb802304e7e63ae41c
TcError <tilman.kurmayer@gmx.de>
2023-04-09 18:27:55 +0200

    added .py file for db_api (first version is finished)

---
+++ db_api.py
@@ -0,0 +1,12 @@
+#############################################
+# USSCS - Universal Server Side Chat System #
+# Version: 0.1.0                            #
+# Author: Tilman Kurmayer                   #
+# License: only with permission from author #
+#                                           #       
+# Info: This file contains the API for the  #
+# database                                  #
+# for the USSCS                             #
+#                                           #
+# LAYER 2                                   #
+#############################################     
