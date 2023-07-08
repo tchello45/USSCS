03bf668e9aafcf88875e2585ad9395f2e17ec6e2
tchello45 <tilman.kurmayer@outlook.de>
2023-07-08 11:44:48 +0200

    setup update

--- usscs/setup.sh
+++
@@ -1,2 +0,0 @@
-python3 $HOME/Documents/USSCS/usscs/setup.py sdist bdist_wheel
-pip3 install $HOME/Documents/USSCS/dist/usscs-3.0.0-py3-none-any.whl --force-reinstall
--- usscs_enc/setup.sh
+++
@@ -1,2 +0,0 @@
-python3 $HOME/Documents/USSCS/usscs_enc/setup.py sdist bdist_wheel
-pip3 install $HOME/Documents/USSCS/dist/usscs_enc-3.0.0-py3-none-any.whl --force-reinstall
